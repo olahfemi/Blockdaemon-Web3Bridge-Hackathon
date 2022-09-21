5224174bf263a2ec46f7c7d8c5d9421f6d5cad40
Abiodun <aabiodunawoyemi@gmail.com>
2022-09-21 12:03:10 +0100

    home page and some other components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      redy: "#f254a3",
+      primary: "#49adec",
+      dark: '#1E2735',
+      dark_deep: "#111623",
+      dark_variant: '#252A39',
+      white: '#ffffff',
+      white_variant: "#9fa7bb"
+    },
+    extend: {
+      boxShadow: {
+        '2xl': '0px 4px 48px rgba(32, 51, 160, 0.08)',
+      },
+      animation: {
+        spin: 'spin 2s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
